562bbb8354e271f9e54835d06208ddbfdbe20f2f
mjthomas22 <mthomas22@gmail.com>
2017-06-30 11:48:35 -0400

    2nd Commit

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -10,6 +10,28 @@ namespace FortuneTeller
     {
         static void Main(string[] args)
         {
+            //Gather users information
+            Console.WriteLine("Please enter your first name.");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name.");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age");
+            int userAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the month you were born 1-12.");
+            int userBirth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the number of siblings you have.");
+            int userSiblings = int.Parse (Console.ReadLine());
+
+            Console.WriteLine("Please enter your favorite ROYGBIV color, if you do not know ROYGBIV please type \"Help\".");
+            string roygbiv = Console.ReadLine();
+            
+            //Check if help is needed
+
+
         }
     }
 }
